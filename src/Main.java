9447c7f8ca0d19823e5d8d684c653e4e426601f7
davidl572 <davidl572@nycstudents.net>
2025-11-10 15:31:39 -0500

    Update Main.java (code didnt save)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void Main(String[]args)
+    {
+        System.out.println(APCalendar.numberofLeapYears(2000,2025));
+        System.out.println(APCalendar.dayOfWeek(1,5,2019));
+        System.out.println(APCalendar.dayOfWeek(1,10,2019));
+    }
 }
