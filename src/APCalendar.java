e9fd59b5a42ded74740216a7c190d7603cb62a69
davidl572 <davidl572@nycstudents.net>
2025-11-10 15:32:10 -0500

    resave code

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
 public class APCalendar {
+    private static boolean isLeapYear(int year) {
+        boolean four = year % 4 == 0;
+        boolean hundred = year % 100 == 0;
+        boolean fourHundred = year % 400 == 0;
+        return four && (!hundred || fourHundred);
+
+    }
+
+    public static int numberofLeapYears(int year1, int year2) {
+        int count = 0;
+        for (int i = year1; i <= year2; i++)
+            if (isLeapYear(i))
+                count++;
+        return count;
+    }
+
+    private static int firstDayOfYear(int year) {
+        int day = 1; // Assume january 1,0001 was a Monday.
+        for (int i = 1; i < year; i++) {
+            day++;
+            if (isLeapYear(i)) day++;
+        }
+        return day % 7;
+    }
+
+    private static int dayofYear(int month, int day, int year) {
+        int days = 0;
+        if (month > 1) days += 31;
+        if (month > 2) days += 28;
+        if (isLeapYear(year)) day++;
+        if (month > 3) days += 31;
+        if (month > 4) days += 30;
+        if (month > 5) days += 31;
+        if (month > 6) days += 30;
+        if (month > 7) days += 31;
+        if (month > 8) days += 31;
+        if (month > 9) days += 30;
+        if (month > 10) days += 31;
+        if (month > 11) days += 30;
+        days += day;
+        return days;
+    }
+public static int dayOfWeek(int month, int day, int year)
+{
+    int days = dayofYear(month, day, year)%7;
+    return firstDayOfYear(year) + days-1;
+}
+
+
 }
