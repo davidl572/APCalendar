c6d000d17276d90898b979bc44a787a8775dd99d
davidl572 <davidl572@nycstudents.net>
2025-11-10 20:18:37 -0500

    fixed bug

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main
     {
-public static void main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println (APCalendar.numberOfLeapYears(2000, 2025));
         System.out.println (APCalendar.dayOfWeek(1, 5, 2019));
